a8f0f01cb900c09fd70724f48cec623d2145d2b4
Jatin Arora <Jatina29@gmail.com>
2017-10-04 18:24:58 +0530

    lombok Added

--- src/main/java/com/DBProject/controller/DefaultController.java
+++ src/main/java/com/DBProject/controller/DefaultController.java
@@ -8,26 +8,16 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Created by Jatin on 02/10/17.
- */
-
 @Controller
 public class DefaultController {
     @RequestMapping(value="/*", method = {RequestMethod.GET})
     public ModelAndView getModelView(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        System.out.println("here");
         return new ModelAndView("index");
     }
-
     @RequestMapping(value="views/*", method = {RequestMethod.GET})
     public ModelAndView getPage(final HttpServletRequest request, final HttpServletResponse response) {
         String servletPath = request.getServletPath();
-        String page = servletPath.substring(servletPath.lastIndexOf("/")+1);
-        System.out.println(page);
+        String page = servletPath.substring(servletPath.lastIndexOf("/") + 1);
         return new ModelAndView(page);
     }
-
-
-
 }
