2ba0cd3f073e9ff88dd22ee8d5a909bb29ff9e84
Jatin Arora <Jatina29@gmail.com>
2017-10-12 04:24:52 +0530

    studentAdded

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
@@ -12,8 +15,15 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "student")
 public class Student implements Serializable {
-    private int id;
-    private int stage;
-    private Double CPI;
+    @Id
+    @Column(name="id")
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private String deptName;
+    @Column
+    private int totalCredits;
 }
--- src/main/java/repository/StudentDAOImpl.java
+++ src/main/java/repository/StudentDAOImpl.java
@@ -1,23 +1,54 @@
 package repository;
 
 import domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Jatin on 06/10/17.
  */
 @Repository
 public class StudentDAOImpl extends SimpleJdbcDaoSupport implements StudentDAO  {
+    @Autowired
+    DataSource dataSource;
+
+    public static Student studentMapper(ResultSet rs) throws SQLException {
+        return new Student(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4));
+    }
+
 
     public void saveStudent(Student student) {
-
+        String insertQuery = "insert into table student values (?, ?, ?, ?);";
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update(insertQuery, new Object[]{student.getId(), student.getName(), student.getDeptName(), student.getTotalCredits()});
     }
 
     public List<Student> getStudents() {
-        return null;
+        String sql  = "select * from student";
+        try(Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Student> students = new ArrayList<>();
+            while(resultSet.next()) {
+                students.add(studentMapper(resultSet));
+            }
+            return students;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
